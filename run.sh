fbb64a162ef0440d68a6e8c1c27ba64394a5e872
YuqiCui <438049712@qq.com>
2019-11-12 12:14:58 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+# run TSK-BN-UR on the Yeast dataset with numer of rules set to 20, the weight for UR will automatically be tuned during training with --weight_frs > 0
 python3 main_diff_data_split.py --data Yeast --bn --n_rules 20 --init kmean --cpu --repeats 5 --weight_frs 1
