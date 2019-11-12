94796a2fdb9a012adbb64000a74840c4c255de2c
YuqiCui <438049712@qq.com>
2019-11-12 12:12:32 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python3 main_diff_data_split.py --data Yeast --bn --n_rules 20 --init kmean --cpu --repeats 5
+python3 main_diff_data_split.py --data Yeast --bn --n_rules 20 --init kmean --cpu --repeats 5 --weight_frs 1
